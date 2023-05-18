4d6359e60dd35f4c46f05432e86fc6f1930bc663
anikakanizesha <xnc7469@autuni.ac.nz>
2023-05-18 12:10:32 +1200

    Add files via upload

---
+++ AdminLogin.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pdc_assignment2;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+/**
+ *
+ * @author anika
+ */
+
+public class AdminLogin extends JFrame{
+    
+    private JPanel contentPane;
+    private JTextField textFieldUserName;
+    private JTextField textFieldPassword;
+    
+    //Launch the application//
+    
+    
+    
+}
